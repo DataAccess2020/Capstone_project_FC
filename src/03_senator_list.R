67a7bbb7e4a9f2fdca64a78cd5a45e0088e12ba1
Francesco Catalfamo <99168620+FCatalfamo@users.noreply.github.com>
2022-03-04 16:59:32 +0100

    Starting question two

--- src/03_senator_list.R
+++ src/03_senator_list.R
@@ -8,3 +8,5 @@ list2 <- list1[[3]][[1]]
 list3 <- list2[[5]]
 
 sen_list <- as.data.frame(do.call(rbind, list3))
+
+sen_list
--- src/08_bill_connection.R
+++ src/08_bill_connection.R
@@ -3,3 +3,10 @@ setwd("/Users/fcata/OneDrive/Desktop/Capstone_project_FC/data/Bill")
 a <- read_xml("BILLS-117s1is.xml")
 
 a <- as_xml_document("BILLS-117s1is.xml")
+
+#sponsor
+xml_child(xml_child(xml_child(xml_child(a, 2), 6), 2), 1)
+#cosponsor
+alfa <- (xml_child(xml_child(xml_child(xml_child(a, 2), 6), 2), 2))
+
+# form - action - action_desc
