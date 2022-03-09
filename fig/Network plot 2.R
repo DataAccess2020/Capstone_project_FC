c71190fddd14193dfe5fc35e9351735d807e2858
Francesco Catalfamo <99168620+FCatalfamo@users.noreply.github.com>
2022-03-10 00:25:31 +0100

    Experiment on the graph

--- fig/Network plot 2.R
+++ fig/Network plot 2.R
@@ -1,13 +1,13 @@
 network <- simpleNetwork(count, height="100px", width="100px",        
                    Source = 1,                 
                    Target = 2,                 
-                   linkDistance = 100,          
-                   charge = -900,                
-                   fontSize = 10,               
+                   linkDistance = 10000,          
+                   charge = -90,                
+                   fontSize = 100,               
                    fontFamily = "serif",       
                    linkColour = "#666",        
-                   nodeColour = "#69b3a2",     
-                   opacity = 0.9,              
+                   nodeColour = "red",     
+                   opacity = 1,              
                    zoom = T                   
 )
 
